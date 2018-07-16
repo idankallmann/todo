4561873315ccc5b70bcee2d8b4b62402b5b91613
idan <idan@develop-soft.com>
2018-07-16 15:10:52 +0300

    fixed db issue

--- server/db/init.js
+++ server/db/init.js
@@ -2,6 +2,6 @@ var Datastore  = require('nedb'),
     db   = {};
 
 db.users = new Datastore({ filename: 'path/to/users', autoload: true });
-db.admins = new Datastore({ filename: 'path/to/admins', autoload: true });
+// db.admins = new Datastore({ filename: 'path/to/admins', autoload: true });
 
 module.exports = db;
